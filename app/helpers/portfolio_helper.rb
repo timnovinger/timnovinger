7519209cd6ada451483f395a86729fbd00c92aaf
Tim Novinger <tim.novinger@gmail.com>
2009-12-15 17:40:50 -0500

    created helper methods for generating portfolio nav links

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,32 +1,29 @@
 # Methods added to this helper will be available to the portfolio templates in the application.
 module PortfolioHelper
 
-    def navLinks
-        a = controller.action_name.underscore
-        
-        if a != "index"
-          #change underscores to spaces and capitalize words in string
-          "#{a}".gsub(/_/, ' ').gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase } + " "
-        end
+    def formatAlt (text)
+       "#{text}".gsub(/-/, ' ').gsub(/^[a-z]|\s+[a-z]/) { |text| text.upcase }
     end
     
     
-#    function navLinks($prev='#', $next='#')
-#    {
-#    	return '<ul id="portfolio_nav">
-#    				<li><a href="portfolio" title="View All">
-#    					<img src="images/icons/bttn_grid.png" alt="portfolio" width="26" height="21" />
-#    				</a></li>
-#    				
-#    				<li><a href="portfolio/'.$prev.'" title="'.ucwords(str_replace('_', ' ', $prev)).'">
-#    					<img src="images/icons/bttn_prev.jpg" alt="previous" width="20" height="21" />
-#    				</a></li>
-#    				
-#    				<li><a href="portfolio/'.$next.'" title="'.ucwords(str_replace('_', ' ', $next)).'">
-#    					<img src="images/icons/bttn_next.jpg" alt="next" width="20" height="21" />
-#    				</a></li>
-#    			</ul>
-#    	';
-#    }
-
+    def navLinks (p, n)
+      altP = formatAlt p
+      
+      altN = formatAlt n
+       
+      links = "<ul id=\"portfolio_nav\">
+         			<li><a href=\"/portfolio\" title=\"View All\">
+         				<img src=\"/images/icons/bttn_grid.png\" alt=\"portfolio\" width=\"26\" height=\"21\" />
+         			</a></li>
+         			
+         			<li><a href=\"/portfolio/"+ p +"\" title=\""+ altP +"\">
+         				<img src=\"/images/icons/bttn_prev.jpg\" alt=\""+ altP +"\" width=\"20\" height=\"21\" />
+         			</a></li>
+         			
+         			<li><a href=\"/portfolio/"+ n +"\" title=\""+ altN +"\">
+         				<img src=\"/images/icons/bttn_next.jpg\" alt=\""+ altN +"\" width=\"20\" height=\"21\" />
+         			</a></li>
+      		   </ul>"
+    end
+    
 end
