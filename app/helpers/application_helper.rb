92588b788c7d9efe269dafcfbb7a314cd1977e83
Tim Novinger <tim.novinger@gmail.com>
2009-12-15 03:00:39 -0500

    modified output of page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
         
         if a != "index"
           #change underscores to spaces and capitalize words in string
-          "#{a}".gsub(/_/, ' ').gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase } + " -"
+          "#{a}".gsub(/_/, ' ').gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase } + " "
         end
     end
     
