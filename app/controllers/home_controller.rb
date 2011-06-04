aed9ece0b8b39b149f6e3b5033754d81d5097e4c
Tim Novinger <tim.novinger@gmail.com>
2011-06-03 23:16:21 -0400

    Update homepage text content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   layout "public"
-  
+
   def index
-    @keywords = "Tim Novinger, consulting, graphic design, graphic, design, freelance, fort wayne, indiana, development, mootools, twitter, mootools, jquery, iphone, ruby, rails, ruby on rails, RoR, screaming monkeys web guild, javascript, php, mysql, developer, advertising, web, print"
-    @description = "A web developer in Fort Wayne, Indiana experienced in the development, design, and content management of web sites."
+    @keywords = "Tim Novinger, consulting, graphic design, graphic, design, freelance, fort wayne, indiana, development, mootools, twitter, mootools, jquery, iphone, ruby, rails, ruby on rails, RoR, screaming monkeys web guild, javascript, php, mysql, developer, advertising, web, print, iOS, mobile notifier, peter hajas, jailbreak, objective-c, apple"
+    @description = "A developer in Fort Wayne, Indiana experienced in web development and design, and interests in iOS development."
   end
 end
