5ca987eba1c3789a24d3a46724f37cead788aee2
Tim Novinger <tim.novinger@gmail.com>
2011-06-04 03:42:33 -0400

    Upgraded core app to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timnovinger::Application.config.secret_token = 'fbce39011ef3f1c0a0b02ad95d6db831cbb0bf4eac49920260bef87820c3b5be715532a5152ea2c54f8cef84c7747d7b718cb4772dc55a5ffea9745dea4841d3'
